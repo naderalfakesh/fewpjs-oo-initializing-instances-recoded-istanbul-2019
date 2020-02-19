0a0e96eb0a998e7c112b3a1b20d44e5015d809bb
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-02-19 22:03:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ class Breakfast{
     this.drink = drink;
   }
 }
+class Lunch{
+  constructor(salad,soup,drink){
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+class Dinner{
+  constructor(salad,soup,entree,dessert){
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
+  }
+}
